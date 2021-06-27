950c5fe04dfcf512e026eca835f3a25b7e98e515
juanmartin1601 <79119851+juanmartin1601@users.noreply.github.com>
2021-06-27 20:29:08 -0300

    primer commit

---
+++ index.ts
@@ -0,0 +1,73 @@
+class Departamento {
+  nombre: string;
+  constructor(nombre: string) {
+    this.nombre = nombre;
+  }
+  getName() {
+    return this.nombre;
+  }
+}
+
+class Piso {
+  nombre: string;
+  departamentos: Departamento[];
+  constructor(nombre: string) {
+    this.nombre = nombre;
+    this.departamentos = [];
+  }
+
+  pushDepartamento(depto: Departamento) {
+    return this.departamentos.push(depto);
+  }
+
+  getDepartamentos(): Departamento[] {
+    return this.departamentos;
+  }
+}
+
+class Edificio {
+  pisos: Piso[];
+  constructor(piso: Piso[]) {
+    this.pisos = piso;
+  }
+  addDepartamentoToPiso(nombreDePiso: string, departamentos: Departamento) {
+    const pisoEncontrado = this.pisos.find((p) => p.nombre == nombreDePiso);
+    return pisoEncontrado.pushDepartamento(departamentos);
+  }
+  getDepartamentosByPiso(nombreDelPiso: string): Departamento[] {
+    const pisoEncontrado = this.pisos.find((p) => p.nombre == nombreDelPiso);
+    return pisoEncontrado.getDepartamentos();
+  }
+}
+
+// no modificar este test
+function testClaseEdificio() {
+  const unPiso = new Piso("planta baja");
+  const otroPiso = new Piso("primer piso");
+  const unEdificio = new Edificio([unPiso, otroPiso]);
+  const deptoUno = new Departamento("depto uno");
+  const deptoDos = new Departamento("depto dos");
+  const deptoTres = new Departamento("depto tres");
+  unEdificio.addDepartamentoToPiso("planta baja", deptoUno);
+  unEdificio.addDepartamentoToPiso("planta baja", deptoDos);
+  unEdificio.addDepartamentoToPiso("planta baja", deptoTres);
+
+  const deptos = unEdificio.getDepartamentosByPiso("planta baja");
+  const deptosEmpty = unEdificio.getDepartamentosByPiso("primer piso");
+
+  if (
+    Array.isArray(deptosEmpty) &&
+    deptosEmpty.length == 0 &&
+    deptos.length == 3 &&
+    deptos[2].getName() == "depto tres"
+  ) {
+    console.log("testClaseBandaApartment passed");
+  } else {
+    throw "testClaseBandaApartment not passed";
+  }
+}
+
+function main() {
+  testClaseEdificio();
+}
+main();
